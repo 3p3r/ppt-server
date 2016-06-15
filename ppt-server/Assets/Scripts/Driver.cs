031f98854b1c363bd83fd394cf924a5c2b28139f
Sepehr Laal <sepehr.laal@gmail.com>
2016-06-15 12:52:05 -0700

    Added a test driver script

---
+++ ppt-server/Assets/Scripts/Driver.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    PptView pptView;
+
+	void Start ()
+    {
+        string path = Path.Combine(PptView.RootPath, "test.pptx");
+        pptView = new PptView(path);
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    void OnDestroy()
+    {
+        if (pptView != null)
+            pptView.Dispose();
+    }
+}
