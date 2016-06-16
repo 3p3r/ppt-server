870a38ea1c96709f24afce4e1645828e37dea1cd
Sepehr Laal <sepehr.laal@gmail.com>
2016-06-16 16:24:26 -0700

    Added Kernel32 API we use (RPM api)

---
+++ ppt-server/Assets/Scripts/Kernel32.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public static class Kernel32
+{
+    const string DllName = "kernel32.dll";
+
+    [DllImport(DllName, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, uint dwSize, out IntPtr lpNumberOfBytesRead);
+}
