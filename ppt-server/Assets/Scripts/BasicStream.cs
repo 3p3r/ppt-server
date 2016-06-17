78dd94b4b2fa83133b31dd9a6cbf04e978bcf1f9
Sepehr Laal <sepehr.laal@gmail.com>
2016-06-17 14:34:07 -0700

    Added a sample script that uses PptStreamer

---
+++ ppt-server/Assets/Scripts/BasicStream.cs
@@ -0,0 +1,26 @@
+﻿using Helios;
+using System.IO;
+using UnityEngine;
+
+public class BasicStream : MonoBehaviour
+{
+    PptStreamer pptStreamer;
+
+	void Start ()
+    {
+        pptStreamer = new PptStreamer(new PptStreamer.LaunchOptions
+        {
+            SlideShowPath = Path.Combine(PptView.RootPath, "test.pptx"),
+            StartSlide = 1,
+            StreamAddress = "127.0.0.1",
+            StreamPort = 10000,
+            StreamWidth = 3840,
+            StreamHeight = 2160
+        });
+	}
+	
+	void OnDestroy ()
+    {
+        pptStreamer.Dispose();
+	}
+}
