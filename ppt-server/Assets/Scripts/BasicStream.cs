fe67684f7428ce9e98c6469a9724ac5d99f0ffb1
Sepehr Laal <sepehr.laal@gmail.com>
2016-06-19 19:56:00 -0700

    replaced hardcoded values

--- ppt-server/Assets/Scripts/BasicStream.cs
+++ ppt-server/Assets/Scripts/BasicStream.cs
@@ -1,5 +1,6 @@
 ﻿using Helios;
 using System.IO;
+using System.Net;
 using UnityEngine;
 
 public class BasicStream : MonoBehaviour
@@ -11,11 +12,11 @@ public class BasicStream : MonoBehaviour
         pptStreamer = new PptStreamer(new PptStreamer.LaunchOptions
         {
             SlideShowPath = Path.Combine(PptView.RootPath, "test.pptx"),
-            StartSlide = 1,
-            StreamAddress = "127.0.0.1",
+            StreamAddress = IPAddress.Loopback.ToString(),
+            StreamHeight = PptView.ScreenHeight,
+            StreamWidth = PptView.ScreenWidth,
             StreamPort = 10000,
-            StreamWidth = 3840,
-            StreamHeight = 2160
+            StartSlide = 1
         });
 	}
 	
